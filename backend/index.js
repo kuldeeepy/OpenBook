6bf2873f9f6573aab7a5f8f5520c3e3ed1cea364
kuldeeepy <kuldeeep.yadav1@gmail.com>
2024-11-12 01:37:49 +0530

    created docker image

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,16 @@ let browser;
 async function launchBrowser() {
     if (!browser) {
         browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--single-process',
+                '--no-zygote',
+            ],
+            executablePath: process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
     }
     return browser;
