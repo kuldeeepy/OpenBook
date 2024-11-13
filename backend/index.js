0022dba340534b7cff5f21971d306a4e10baa593
kuldeeepy <kuldeeep.yadav1@gmail.com>
2024-11-13 10:23:46 +0530

    prep to deploy

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,37 @@ const base = process.env.BASE_URL
 const PORT = process.env.PORT 
 
 
+app.get("/wakeup", (req, res) => res.send("Server is awake!"));
+
+app.get("/search", async (req, res) => {
+    
+    const query = req.query.q;
+    if (!query) return res.status(400).json({Error: "Missing query param 'q'"});
+
+    const books = await searchBooks(query);
+    if (!books || books.length === 0) return res.status(404).json({Error: "Book not found, try different one!"});
+
+    res.json(books);
+})
+
+app.get("/ebook", async (req, res) => {
+
+    const {url} = req.query;
+    if (!url) return res.status(400).json({error: "Missing query 'q"});
+
+    const downloadUrl = await getDownloadLink(url);
+    if (!downloadUrl) return res.status(404).json({Error: "Ebook not found, try different one!"});
+
+    res.json(downloadUrl)
+})
+
+
 async function searchBooks(q) {
 
     let browser = null;
     try {
         browser = await puppeteer.launch({
-            headless: true,
+            headless: "new",
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
@@ -22,10 +47,11 @@ async function searchBooks(q) {
                 : puppeteer.executablePath(),
         });
         const page = await browser.newPage();
-        // await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
-        await page.goto(`${base}/search?q=${encodeURIComponent(q)}&page=1`, { waitUntil: 'networkidle2', timeout: 90000});
-
-        const books = await page.evaluate((baseUrl) => {
+        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+        await page.goto(`${base}/search?q=${encodeURIComponent(q)}&page=1`, { waitUntil: 'networkidle2', timeout: 50000});
+        // await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+        // await page.waitForSelector('.files-new ul li', {waitUntil: "networkidle0"})
+        const books = await page.evaluate(async (baseUrl) => {
             const result = [];
             const bookElements = document.querySelectorAll(".files-new ul li");
 
@@ -46,7 +72,7 @@ async function searchBooks(q) {
         }, base);
         return books;
     } catch (error) {
-        console.error("Error fetching books:", error);
+        handleErrors(error, "searchBooks")
     } finally {
         if (browser) await browser.close();
     }
@@ -56,41 +82,27 @@ async function getDownloadLink(bookUrl) {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(bookUrl, { waitUntil: 'networkidle2', timeout: 50000 });
+    await page.goto(bookUrl, { waitUntil: 'networkidle2', timeout: 30000 });
 
     const initialDownloadSelector = '#download-button > a';
-    await page.waitForSelector(initialDownloadSelector, { visible: true, timeout: 90000 });
+    await page.waitForSelector(initialDownloadSelector, { visible: true, timeout: 30000 });
     const initialDownloadLink = await page.$eval(initialDownloadSelector, element => element.href);
 
     await page.goto(initialDownloadLink, { waitUntil: 'networkidle2' });
 
     const finalDownloadSelector = '.btn-group a[type="button"]';
-    await page.waitForSelector(finalDownloadSelector, { visible: true, timeout: 50000 });
-    const finalDownloadLink = await page.$eval(finalDownloadSelector, element => element.href);
+    await page.waitForSelector(finalDownloadSelector, { visible: true, timeout: 30000 });
+    let finalDownloadLink = await page.$eval(finalDownloadSelector, element => element.href);
 
+    finalDownloadLink = finalDownloadLink?.split('ext')[0] + "ext=pdf"
     await browser.close();
     return {finalDownloadLink}
 }
 
-app.get("/searchbooks", async (req, res) => {
-    
-    const query = req.query.q;
-    if (!query) return res.status(400).json({error: "Missing query param 'q'"});
+const handleErrors = (error, functionName) => {
+    console.error(`Error in ${functionName}:`, error.message);
+};
 
-    const books = await searchBooks(query);
-    res.json(books);
-})
-
-app.get("/ebook", async (req, res) => {
-
-    const query = req.query.q;
-    if (!query) return res.status(400).json({error: "Missing query 'q"});
-
-    const downloadUrl = await getDownloadLink(query);
-    res.json(downloadUrl)
-})
-
-app.get("/wakeup", (req, res) => res.send("Server is awake!"));
 
 app.listen(PORT, () => {    
     console.log(`Server running on port ${PORT}`);
